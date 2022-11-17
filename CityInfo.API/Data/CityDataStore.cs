972b6704c5c19be5c0af6d1467ad4f2f08aca482
Laura Voineag <laura_voineag@waters.com>
2022-11-17 08:05:40 +0000

    Add Model Class and Data Class with Dummy Data

---
+++ CityInfo.API/Data/CityDataStore.cs
@@ -0,0 +1,34 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API.Data;
+
+public class CityDataStore
+{
+    public List<City> Cities { get; set; }
+
+    public CityDataStore()
+    {
+        //init dummy data
+        Cities = new List<City>()
+        {
+            new City()
+            {
+                Id = 1,
+                Name = "Manchester",
+                Description = "The one where I live."
+            },
+            new City()
+            {
+                Id = 2,
+                Name = "London",
+                Description = "The one that is really crowded."
+            },
+            new City()
+            {
+                Id = 3,
+                Name = "Liverpool",
+                Description = "The one where Beatles are from"
+            }
+        };
+    }
+}
